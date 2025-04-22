e4e05a56fe96a2722210b5657a878de7933288ec
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2025-04-22 11:09:20 +0200

    Deleted unused files

--- Prosjektoppgave-218/Program.cs
+++ Prosjektoppgave-218/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Map}/{action=Map}/{id?}");
 
 app.Run();
