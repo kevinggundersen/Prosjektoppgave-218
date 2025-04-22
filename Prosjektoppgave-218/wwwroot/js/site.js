10eac4ddd697a242df98429821f352a13a3914b0
Kreline1993 <n.engemann1993@gmail.com>
2025-04-22 11:16:19 +0200

    slettet kartlag som brukes ikke

--- Prosjektoppgave-218/wwwroot/js/site.js
+++ Prosjektoppgave-218/wwwroot/js/site.js
@@ -10,26 +10,7 @@ var kartverket = L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/topo/def
     attribution: '&copy; Kartverket</a>'
 }).addTo(map);
 
-// Tilelayer for wind resources
-var windSource =L.tileLayer.wms("https://nve.geodataonline.no/arcgis/services/Vindressurser/MapServer/WMSServer", {
-    // Replace '0' with the actual layer name as defined in the GetCapabilities document
-    layers: "Gj.snittlig_vindstyrke_120m_over_bakkeniva",
-    format: "image/png",
-    transparent: true,
-    attribution: "NVE Geodata Online",
-    opacity: 1
-}).addTo(map);
 
-// Add the WMS layer for terrain complexity
-var terrainComp = L.tileLayer.wms("https://nve.geodataonline.no/arcgis/services/Vindressurser/MapServer/WMSServer", {
-    // Replace '0' with the actual layer name as defined in the GetCapabilities document
-    layers: "Terrengkompleksitet_RIX",
-    format: "image/png",
-    transparent: true,
-    attribution: "NVE Geodata Online",
-    opacity: 1,
-    className: 'blend-layer'
-}).addTo(map);
 
 //Google labels
 var googleLabels = L.tileLayer('https://mt1.google.com/vt/lyrs=h&x={x}&y={y}&z={z}', {
@@ -42,16 +23,10 @@ var baseMaps = {
     "kartverket": kartverket
 };
 
-map.addLayer(windSource);
-map.addLayer(terrainComp);
 map.addLayer(googleLabels);
 
 var overlayMaps = {
-    "wind Source": windSource,
-    "terrain Complexity": terrainComp,
     "google Labels": googleLabels
 };
 
 var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
-
-map.removeLayer(terrainComp);
